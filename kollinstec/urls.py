30a1d4e1911bdecbf3473c728055b20bac706cd1
Éderson Cássio <edersoncassiolf@outlook.com>
2023-05-14 10:54:27 -0300

    Webhook de confirmação do pagamento

--- kollinstec/urls.py
+++ kollinstec/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("comprar-match/<str:jogo>", views.comprar_match, name="comprar_match"),
-    path("pagar/<str:tipo>/<str:nome>", views.pagar, name="comprar_match"),
+    path("pagar/<str:tipo>/<str:nome>", views.pagar, name="pagamento"),
+    path("processar-pagamento", views.payment_webhook, name="payment_webhook"),
 ]
--- kollinstec/views.py
+++ kollinstec/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
 
 import stripe
 
-stripe.api_key = 'sk_test_51N492lGdLPxaw6poID4phJ72YwG7ZoZtQkWVDBRVqG2ugBSTGwpSyDEXq1mnjb4wDlNRNK5gXrTBWrCHWGbgkibS00qudpJ3gM'
+# TODO 
+# configuar keys via envvars
+# refatorar o código
+
+stripe.api_key = '...'
+
+# dado pela Stripe CLI
+webhook_secret = '...'
 
 PAYMENT_LINK = 'https://buy.stripe.com/test_cN2bLsgUU8PP4PC144'
 
@@ -25,7 +33,7 @@ def pagar(request, tipo, nome):
             {
                 "price_data": {
                     "currency": "brl",
-                    "unit_amount": 1500,  # em 1/100
+                    "unit_amount": 1500,  # R$ 15,00
                     "product_data": {
                         # buscamos no BD a partir de tipo e nome
                         "name": nome,
@@ -35,7 +43,32 @@ def pagar(request, tipo, nome):
                 "quantity": 1,
             },
         ],
+        metadata={
+            "name": nome,
+            "description": tipo,
+        },
         mode="payment",
     )
     return HttpResponseRedirect(session.url)
 
+@csrf_exempt
+def payment_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(payload, sig_header, webhook_secret)
+    except ValueError as e:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        return HttpResponse(status=400)
+
+    if event['type'] == 'checkout.session.completed':
+        session = stripe.checkout.Session.retrieve(
+            event['data']['object']['id'], expand=['line_items'])
+
+        # Aqui processamos a venda
+        print(session.metadata)
+
+    return HttpResponse('de hoje tá pago')
